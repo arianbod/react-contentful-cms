7ee01491d40da5712cfff8a5f524f0e9fe0e42d8
Arian <Arianbod@outlook.com>
2024-02-10 18:19:37 +0300

    Hero Created

---
+++ src/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Hero = () => {
+	return (
+		<section className='hero'>
+			<div className='hero-center'>
+				<div className='hero-title'>
+					<h1>Contentful CMS</h1>
+					<p>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas unde
+						odio harum consectetur molestiae deserunt ratione. Ducimus, vero
+						amet eius repudiandae quam non tempora, inventore eum praesentium
+						voluptates nulla. Quisquam.
+					</p>
+				</div>
+			</div>
+			;
+		</section>
+	);
+};
+
+export default Hero;
