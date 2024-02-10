d64da9dd6b1cbccd68ce7693c9e9eac6f7cc7641
Arian <Arianbod@outlook.com>
2024-02-10 23:59:52 +0300

    specialized to me

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -5,12 +5,13 @@ const Hero = () => {
 		<section className='hero'>
 			<div className='hero-center'>
 				<div className='hero-title'>
-					<h1>Contentful CMS</h1>
+					<h1>Amin(Arian) Abtahi</h1>
 					<p>
-						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas unde
-						odio harum consectetur molestiae deserunt ratione. Ducimus, vero
-						amet eius repudiandae quam non tempora, inventore eum praesentium
-						voluptates nulla. Quisquam.
+						Skilled Front End Developer with expertise in modern web
+						technologies and user interface design. Proven track record in
+						leading development teams and executing large-scale projects,
+						including a major e-learning platform. Committed to delivering
+						efficient and scalable front-end solutions
 					</p>
 				</div>
 				<div className='img-container'>
