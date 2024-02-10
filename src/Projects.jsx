ddeaac35268bb8b465cc0891e1220dcba08da83e
Arian <Arianbod@outlook.com>
2024-02-10 23:27:13 +0300

    generated

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
-
+import { useFetchProjects } from './fetchProjects';
 const Projects = () => {
-	return <h2>Projects</h2>;
+	const { isLoading, projects } = useFetchProjects();
+
+	return isLoading ? (
+		<section className='projects'>
+			<h2>Loading...</h2>
+		</section>
+	) : (
+		<section className='projects'>
+			<div className='title'>
+				<h2>projects</h2>
+				<div className='title-underline'></div>
+				<div className='projects-center'>
+					{projects.map(({ id, img, url, title }) => {
+						return (
+							<a
+								key={id}
+								href={url}
+								target='_blank'
+								rel='norefererrer'
+								className='project'>
+								<img
+									src={img}
+									alt={title}
+									className='img'
+								/>
+								<h5>{title}</h5>
+							</a>
+						);
+					})}
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Projects;
